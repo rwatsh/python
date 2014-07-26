e676352e91372388ff708fad2fc591e690ee19f5
Watsh Rajneesh <rwatsh@gmail.com>
2014-07-26 00:11:39 -0700

    Completed battleship commit

--- codeacademy_proj/codeacademy/battleship.py
+++ codeacademy_proj/codeacademy/battleship.py
@@ -32,6 +32,32 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board) - 1)
 
-random_row(board)
-random_col(board)
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+print ship_row
+print ship_col
+
+# Player has 4 turns to guess right or else the game is over.
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Col: "))
+
+    if guess_row not in range(5) or guess_col not in range(5):
+            print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == "X":
+            print "You guessed that one already."
+    else:
+        if guess_row == ship_row and guess_col == ship_col:
+            print "Congratulations! You sank my battleship!"
+            break
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = 'X'
+            print "Turn", turn + 1
+            print_board(board)
+    if turn == 3:
+            print "Game Over"
+
+
 
